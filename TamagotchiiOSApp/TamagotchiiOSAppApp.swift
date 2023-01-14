cc18e2b4c81c623013d5875330912f7ae0b4f839
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2023-01-14 10:57:18 +0000

    Initial Commit

---
+++ TamagotchiiOSApp/TamagotchiiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiiOSAppApp.swift
+//  TamagotchiiOSApp
+//
+//  Created by Jaswal, Eesa (SPH) on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
